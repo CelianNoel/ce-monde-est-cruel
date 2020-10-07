38a4bd59e259e5f60145c9b25e12fd699c92354b
CelianNoel <celian.noel164@gmail.com>
2020-10-07 21:55:37 +0200

    dummy

--- src/Game/PlayerIA/PipadaPlayer.php
+++ src/Game/PlayerIA/PipadaPlayer.php
@@ -8,6 +8,7 @@ use Hackathon\Game\Result;
  * Class PipadaPlayers
  * @package Hackathon\PlayerIA
  * @author Celian NOEL
+ * Strat : L'onjectif est de counter une IA idiote qui cherche juste a gagner au prochain coup par rapport a ce qui a été joué avant
  */
 class PipadaPlayer extends Player
 {
@@ -21,13 +22,22 @@ class PipadaPlayer extends Player
         $oponentlast = $this->result->getLastChoiceFor($this->opponentSide);
         $lastscore = $this->result->getLastScoreFor($this->mySide);
         
-
+        if ($lastscore < 1)
+        {
             if ( $oponentlast == "rock"){
-                return parent::rockChoice();
-            }
-            if ($oponentlast == "paper"){
                 return parent::paperChoice();
             }
-            return parent::scissorsChoice();
+            if ($oponentlast == "paper"){
+                return parent::scissorsChoice();
+            }
+            return parent::rockChoice();
+        }
+        if ( $oponentlast == "rock"){
+            return parent::rockChoice();
+        }
+        if ($oponentlast == "paper"){
+            return parent::paperChoice();
+        }
+        return parent::scissorsChoice();
     }
 };
